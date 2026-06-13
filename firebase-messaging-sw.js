5324aca4fb921a9b5bb4cb79b7a15002ea9cb301
diyas-0707 <diyasureshkumar77@gmail.com>
2026-06-13 15:08:35 -0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,52 +1,22 @@
-cat << 'EOF'
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyExample", // will fill from your config
+  apiKey: "AIzaSyD8UJlf7N95PSfbWIFmKRkww-qduDcExp0",
   authDomain: "bharatanatyam-tracker.firebaseapp.com",
   projectId: "bharatanatyam-tracker",
-  storageBucket: "bharatanatyam-tracker.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  storageBucket: "bharatanatyam-tracker.firebasestorage.app",
+  messagingSenderId: "596130424146",
+  appId: "1:596130424146:web:548447703d6efc920a868b"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-  const { title, body, icon } = payload.notification;
-  self.registration.showNotification(title, {
-    body,
-    icon: icon || '/icon-192.png',
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/icon-192.png',
     badge: '/icon-192.png',
     data: payload.data
   });
 });
-EOF
-echo "done"
-Output
-
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-  apiKey: "AIzaSyExample", // will fill from your config
-  authDomain: "bharatanatyam-tracker.firebaseapp.com",
-  projectId: "bharatanatyam-tracker",
-  storageBucket: "bharatanatyam-tracker.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(payload => {
-  const { title, body, icon } = payload.notification;
-  self.registration.showNotification(title, {
-    body,
-    icon: icon || '/icon-192.png',
-    badge: '/icon-192.png',
-    data: payload.data
-  });
-});
-done
