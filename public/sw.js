446d0036e03c27dcd9ff426d0a6de3b879048282
diyas-0707 <diyasureshkumar77@gmail.com>
2026-06-13 15:42:59 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,12 +4,13 @@ self.addEventListener('push', e => {
     self.registration.showNotification(data.title || 'Bharatanatyam Tracker', {
       body: data.body || '',
       icon: '/icon-192.png',
-      badge: '/icon-192.png'
+      badge: '/icon-192.png',
+      data: { url: data.url || '/' }
     })
   );
 });
 
 self.addEventListener('notificationclick', e => {
   e.notification.close();
-  e.waitUntil(clients.openWindow('/'));
+  e.waitUntil(clients.openWindow(e.notification.data.url));
 });
